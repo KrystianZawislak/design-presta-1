d7f0b3e7b042d946de8f3ccaf9276ef93979defe
KrystianZawislak <kryzawdev@gmail.com>
2026-09-08 11:22:15 +0200

    Add designassets module: auto-register theme layout CSS files

---
+++ prestashop/modules/designassets/designassets.php
@@ -0,0 +1,57 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class DesignAssets extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'designassets';
+        $this->tab = 'front_office_features';
+        $this->author = 'Design Presta';
+        $this->version = '1.0.0';
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Design Assets');
+        $this->description = $this->l('Registers the theme layout CSS files.');
+
+        $this->ps_versions_compliancy = ['min' => '8.0.0', 'max' => _PS_VERSION_];
+    }
+
+    public function install()
+    {
+        return parent::install() && $this->registerHook('actionFrontControllerSetMedia');
+    }
+
+    public function hookActionFrontControllerSetMedia()
+    {
+        $layoutDir = _PS_THEME_DIR_ . 'assets/css/layout/';
+
+        if (!is_dir($layoutDir)) {
+            return;
+        }
+
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($layoutDir, FilesystemIterator::SKIP_DOTS)
+        );
+
+        foreach ($files as $file) {
+            if ($file->getExtension() !== 'css') {
+                continue;
+            }
+
+            $relativePath = '/assets/css/layout/' . substr($file->getPathname(), strlen($layoutDir));
+            $id = 'designassets-' . str_replace(['/', '\\'], '-', substr($relativePath, strlen('/assets/css/layout/')));
+
+            $this->context->controller->registerStylesheet(
+                $id,
+                $relativePath,
+                ['media' => 'all', 'priority' => 60]
+            );
+        }
+    }
+}
